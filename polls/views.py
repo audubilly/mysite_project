be93d26186f85c1335562176537a7c144ab94b81
Audu Billy <audubilly89@gmail.com>
2021-04-27 15:13:00 +0100

    setup main branch

---
+++ polls/views.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from polls.models import Question, Choice
+
+
+def index(request):
+    questions = Question.objects.all()
+
+    context = {
+        "questions": questions,
+
+    }
+    # print(questions[0].choice_set.all())
+    return render(request, "polls/index.html", context)
